d7acdbf330bb0ddce9cf03df7588d24759537452
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2023-01-08 22:03:43 +0200

    Topic: Render List with Section list

--- week_2/components/MenuItems.js
+++ week_2/components/MenuItems.js
@@ -1,29 +1,49 @@
 import React from 'react';
 
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, SectionList } from 'react-native';
 
 const menuItemsToDisplay = [
-    { name: 'Hummus', price: '$5.00', id: '1A' },
-    { name: 'Moutabal', price: '$5.00', id: '2B' },
-    { name: 'Falafel', price: '$7.50', id: '3C' },
-    { name: 'Marinated Olives', price: '$5.00', id: '4D' },
-    { name: 'Kofta', price: '$5.00', id: '5E' },
-    { name: 'Eggplant Salad', price: '$8.50', id: '6F' },
-    { name: 'Lentil Burger', price: '$10.00', id: '7G' },
-    { name: 'Smoked Salmon', price: '$14.00', id: '8H' },
-    { name: 'Kofta Burger', price: '$11.00', id: '9I' },
-    { name: 'Turkish Kebab', price: '$15.50', id: '10J' },
-    { name: 'Fries', price: '$3.00', id: '11K' },
-    { name: 'Buttered Rice', price: '$3.00', id: '12L' },
-    { name: 'Bread Sticks', price: '$3.00', id: '13M' },
-    { name: 'Pita Pocket', price: '$3.00', id: '14N' },
-    { name: 'Lentil Soup', price: '$3.75', id: '15O' },
-    { name: 'Greek Salad', price: '$6.00', id: '16Q' },
-    { name: 'Rice Pilaf', price: '$4.00', id: '17R' },
-    { name: 'Baklava', price: '$3.00', id: '18S' },
-    { name: 'Tartufo', price: '$3.00', id: '19T' },
-    { name: 'Tiramisu', price: '$5.00', id: '20U' },
-    { name: 'Panna Cotta', price: '$5.00', id: '21V' },
+    {
+        title: 'Appetizers',
+        data: [
+            { name: 'Hummus', price: '$5.00' },
+            { name: 'Moutabal', price: '$5.00' },
+            { name: 'Falafel', price: '$7.50' },
+            { name: 'Marinated Olives', price: '$5.00' },
+            { name: 'Kofta', price: '$5.00' },
+            { name: 'Eggplant Salad', price: '$8.50' },
+        ],
+    },
+    {
+        title: 'Main Dishes',
+        data: [
+            { name: 'Lentil Burger', price: '$10.00' },
+            { name: 'Smoked Salmon', price: '$14.00' },
+            { name: 'Kofta Burger', price: '$11.00' },
+            { name: 'Turkish Kebab', price: '$15.50' },
+        ],
+    },
+    {
+        title: 'Sides',
+        data: [
+            { name: 'Fries', price: '$3.00', id: '11K' },
+            { name: 'Buttered Rice', price: '$3.00' },
+            { name: 'Bread Sticks', price: '$3.00' },
+            { name: 'Pita Pocket', price: '$3.00' },
+            { name: 'Lentil Soup', price: '$3.75' },
+            { name: 'Greek Salad', price: '$6.00' },
+            { name: 'Rice Pilaf', price: '$4.00' },
+        ],
+    },
+    {
+        title: 'Desserts',
+        data: [
+            { name: 'Baklava', price: '$3.00' },
+            { name: 'Tartufo', price: '$3.00' },
+            { name: 'Tiramisu', price: '$5.00' },
+            { name: 'Panna Cotta', price: '$5.00' },
+        ],
+    },
 ];
 
 const Item = ({ name, price }) => (
@@ -36,12 +56,19 @@ const Item = ({ name, price }) => (
 const MenuItems = () => {
     const renderItem = ({ item }) => <Item name={item.name} price={item.price} />;
 
+    const sectionHeader = ({ section: { title } }) => (
+        <View style={menuStyles.headerStyle}>
+            <Text style={menuStyles.sectionHeader}>{title}</Text>
+        </View>
+    );
+
     return (
         <View style={menuStyles.container}>
-            <FlatList
-                data={menuItemsToDisplay}
-                keyExtractor={(item) => item.id}
-                renderItem={renderItem}></FlatList>
+            <SectionList
+                sections={menuItemsToDisplay}
+                keyExtractor={(item, index) => item + index}
+                renderItem={renderItem}
+                renderSectionHeader={sectionHeader}></SectionList>
         </View>
     );
 };
@@ -59,6 +86,15 @@ const menuStyles = StyleSheet.create({
     itemText: {
         color: '#F4CE14',
         fontSize: 20,
+    },
+    headerStyle: {
+        backgroundColor: '#F4CE14',
+    },
+    sectionHeader: {
+        color: 'black',
+        fontSize: 26,
+        flexWrap: 'wrap',
+        textAlign: 'center',
     },
 });
 
